ffcbd90b6af034fbd7d2f2dd970ac83e23cb426b
Josue Gutierrez <joshlink0@gmail.com>
2023-11-01 16:23:28 -0600

    9 Val & Var 10 Prac Assign ++ Assignment PDF

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,16 @@
-let js="amazing";
-if(js==="amazing")alert("JavaScript is FUN!");
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
 
-40+8+23+10;
-console.log(40+8+23-10);
+40 + 8 + 23 + 10;
+console.log(40 + 8 + 23 - 10);
 
-let firstName="Jonas";
+let firstName = "Jonas";
 console.log(firstName);
 
+let country = "Mexico";
+let continent = "America";
+let population = 126.7;
+
+console.log(country);
+console.log(continent);
+console.log(population);
